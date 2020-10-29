4469e9f7be2df798dd84b306efeb831a7430da2b
Gabriel Quiroz <gabriel@mb.company>
2020-10-28 19:06:09 -0600

    DONE-> BADO-4
    endpoint de /sorteos

--- api/serializers.py
+++ api/serializers.py
@@ -10,12 +10,21 @@ class CartaSerializer(serializers.ModelSerializer):
 
 
 class SorteoSerializer(serializers.ModelSerializer):
+    imagen = serializers.SerializerMethodField()
+
+    def get_imagen(self, obj):
+        querysetI = Imagen.objects.filter(sorteo=obj.id, principal=True).values('imagen')[:1]
+        dato = None
+        for datos in querysetI:
+            dato = datos['imagen']
+        return dato
+
     class Meta:
         model = Sorteo
-        fields = '__all__'
+        fields = ['id', 'titulo', 'descripcion', 'detalles', 'activo', 'fechaHoraSorteo', 'imagen']
 
 
 class ImagenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Imagen
-        fields = '__all__si'
+        fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import *
 
@@ -11,5 +11,10 @@ urlpatterns = [
     path('cartas/detail/<pk>/', CartaDetailView.as_view(), ),
     path('cartas/update/<pk>/', CartaUpdateView.as_view(), ),
     path('cartas/delete/<pk>/', CartaDeleteView.as_view(), ),
-
+    # ----------------------------------------------------------------------------------Sorteo
+    path('sorteos/create/', SorteoCreateView.as_view(), ),
+    path('sorteos/list/', SorteoListView.as_view(), ),
+    path('sorteos/detail/<pk>/', SorteoDetailView.as_view(), ),
+    path('sorteos/update/<pk>/', SorteoUpdateView.as_view(), ),
+    path('sorteos/delete/<pk>/', SorteoDeleteView.as_view(), ),
 ]
