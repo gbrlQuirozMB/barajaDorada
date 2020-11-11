00357eb199dcbf30e9cb28417bb3af205c495eae
Gabriel Quiroz <gabriel@mb.company>
2020-11-11 17:38:43 -0600

    FIXED-> CSFR v2

--- server/settings.py
+++ server/settings.py
@@ -58,7 +58,9 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
